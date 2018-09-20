7f127e290168a256529892b083b920e690d7caaf
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-09-20 11:41:25 +0530

    Create 238.java

---
+++ 238.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 String str;
+Scanner sc=new Scanner(System.in);
+str=sc.nextLine();
+        
+ 
+        String strWithoutSpace = str.replaceAll("\\s","");
+ 
+        System.out.println(strWithoutSpace);         
+ 
+       
+ 
+       
+	}
+}
